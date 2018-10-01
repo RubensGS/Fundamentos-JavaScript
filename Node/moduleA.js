3c959d1b4b1611509cf265028b6c4d83befcb9d1
Rubens Gonçalves De Souza <rgoncalves@indracompany.com>
2018-10-01 11:08:37 -0300

    Modulos Node

---
+++ Node/moduleA.js
@@ -0,0 +1,8 @@
+// EM NODE UM ARQUIVO SIGNIFICA UM MÓDULO
+// TUDO QUE ESCREVE DENTRO DE UM MÓDULO, NÃO FICA VISÍVEL PARA OUTROS AQUIVOS
+// EXXPORTAR CÓDIGO PARA SER USADO EM OUTROS ARQUIVOS
+
+this.ola = 'Fala Pessoal'
+exports.bemVindo = 'Bem Vindo ao Node!'
+module.exports.ateLogo = 'Até o próximo exemplo'
+
